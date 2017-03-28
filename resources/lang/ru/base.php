0a6d58455a1e1b9f25c7f907f2186cd864937852
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-03-28 13:10:01 +0300

    make contact page

--- resources/lang/ru/base.php
+++ resources/lang/ru/base.php
@@ -12,33 +12,15 @@ return [
 	| you want to customize your views to better match your application.
 	|
 	*/
-	'social' => 'в соцсетях',
-	'check_visa' => 'Заказать визу',
-	'info' => 'дополнительная информация',
-	'registry_visa_center' => 'регистрация в визовые центры',
-	'registry' => 'Регистрация',
-	'news' => 'новости',
-	'news_soc' => 'следите за нашими новостями в социальных сетях:',
 	'more' => 'Подробнее',
-	'subscribe_news' => 'подписаться на новости',
-	'insurance' => 'Страховка',
-	'order_insurance' => 'Заказать страховку',
-	'services' => 'другие услуги',
-	'callback' => 'Обратная связь',
-	'advices' => 'Полезные советы',
-	'learn_more' => 'Узнать больше',
-	'want' => 'как нас найти?',
+	'address' => 'Адрес',
+	'want' => 'Как нас найти?',
 	'contact' => 'Контакты',
-	'put_name' => 'Введите Ваше имя:',
+	'callback' => 'Обратная связь',
+	'put_name' => 'Имя',
 	'put_email' => 'Введите Ваш e-mail:',
-	'put_phone' => 'Введите Ваш телефон:',
+	'put_phone' => 'Телефон',
+	'phones' => 'Телефоны',
 	'message' => 'Сообщение',
 	'send' => 'Отправить',
-	'days' => 'дней',
-	"from" => 'от',
-	/* Script Contact*/
-	'success_send_contact' => 'Ваша заявка успешно отправлена !',
-	'error_send_contact' => 'Возникла ошибка при передачи заявки!',
-	/* /Script Contact*/
-
 ];
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ой
- * Date: 26.03.2017
- * Time: 23:38
- */
+@extends('ws-app')
+@section('content')
+
+    <div class="map">
+        <iframe src="{{ $texts->get('map') }}" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+    <div class="callback-wrap">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <h1 class="callback-name">{{ trans('base.callback') }}</h1>
+                </div>
+
+                <div class="col-md-9">
+
+                    <form action="" class="row callback-form">
+
+                        <div class="col-md-6">
+                            <input id="name" name="name" placeholder="{{ trans('base.put_name') }}" required type="text">
+                        </div>
+
+                        <div class="col-md-6">
+                            <input id="phone" name="phone" placeholder="{{ trans('base.put_phone') }}" required type="tel">
+                        </div>
+
+                        <div class="col-md-12">
+                            <textarea name="comment" id="comment" placeholder="{{ trans('base.message') }}" rows="4"></textarea> <br>
+                        </div>
+
+                        <div class="col-md-12">
+                            <button>{{ trans('base.send') }}</button>
+                        </div>
+                    </form>
+
+                </div>
+
+                <div class="col-md-3">
+
+                    <div class="contact-block">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <div class="contact-block_name">{{ trans('base.address') }}</div>
+                        <div class="contact-block_content">{{ $texts->get('address') }}</div>
+                    </div>
+
+                    <div class="contact-block">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <div class="contact-block_name">{{ trans('base.phones') }}</div>
+                        <div class="contact-block_content">{{ $texts->get('telephone_one') }}<br>{{ $texts->get('telephone_two') }}<br>{{ $texts->get('telephone_three') }}</div>
+                    </div>
+
+                    <div class="contact-block">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <div class="contact-block_name">E-mail</div>
+                        <div class="contact-block_content">{{ $texts->get('mail') }}</div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
