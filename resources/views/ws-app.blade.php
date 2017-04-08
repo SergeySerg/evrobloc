717e764e9537cc09cdef348b416ef9bd02e05cbe
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-04-08 17:38:38 +0300

    DB DUMP put item and asset liveinternet

--- resources/views/ws-app.blade.php
+++ resources/views/ws-app.blade.php
@@ -211,6 +211,20 @@
 							src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCXSIuFGADtHtXySwrTW1Ime_ISq33M3aY&callback=initMap">
 					</script>
 				</div>
+				<div class="col-md-12">
+				<!--LiveInternet counter-->
+				<script type="text/javascript">
+					document.write("<a href='//www.liveinternet.ru/click' "+
+							"target=_blank><img src='//counter.yadro.ru/hit?t26.6;r"+
+							escape(document.referrer)+((typeof(screen)=="undefined")?"":
+							";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+									screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+							";"+Math.random()+
+							"' alt='' title='LiveInternet: показано число посетителей за"+
+							" сегодня' "+
+							"border='0' width='88' height='15'><\/a>")
+				</script><!--/LiveInternet-->
+				</div>
 			</div>
 		</div>
 	</footer>
